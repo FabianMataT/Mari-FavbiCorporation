1f59c3ca39236da0823777e3edd5dcaa42df3fa1
FabianMataT <145735103+FabianMataT@users.noreply.github.com>
2025-07-27 20:27:00 -0600

    First commit

---
+++ Mari&FabiCorporation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mari_FabiCorporation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
